cf00856458f65720344ed6aba2cf66ef9b07bfe1
franklin N'TESSAH <dntessah@gmail.com>
2024-11-12 13:37:20 +0100

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://laconciergerie-i-carre.com/testApi/api/',
-  Url: 'https://laconciergerie-i-carre.com/testApi',
+  apiUrl: 'https://laconciergerie-i-carre.com/gestoneApi/api/',
+  Url: 'https://laconciergerie-i-carre.com/gestoneApi',
   wsUrl: 'wss://laconciergerie-i-carre.com/gemstoneApi/ws/reservations/'  // WebSocket URL
 };
